cc7d71f72d926ced77c3782e148deb368b619ff7
Ander Aguinagalde <ander@tisalabs.com>
2019-11-19 18:18:23 +0000

    Insert 5 entries per table

--- insert.sql
+++ insert.sql
@@ -76,10 +76,52 @@ VALUES ("Teen", "3");
 INSERT INTO event_is_organized_for_section_type (section_type_code, event_id)
 VALUES ("Adolescent", "4");
 
-
 /* VOLUNTEER */
+INSERT INTO volunteer (volunteer_id, volunteer_name, volunteer_address, volunteer_phone_number, date_of_birth, volunteer_gender, vetting_completion_date, troop_name)
+VALUES ("0", "Volunteer0", "Volunteer address 0", "000000000", "1995-1-19", "male", "2015-2-06", "troop0");
+
+INSERT INTO volunteer (volunteer_id, volunteer_name, volunteer_address, volunteer_phone_number, date_of_birth, volunteer_gender, vetting_completion_date, troop_name)
+VALUES ("1", "Volunteer1", "Volunteer address 1", "111111111", "1995-1-19", "female", "2015-2-06", "troop1");
+
+INSERT INTO volunteer (volunteer_id, volunteer_name, volunteer_address, volunteer_phone_number, date_of_birth, volunteer_gender, vetting_completion_date, troop_name)
+VALUES ("2", "Volunteer2", "Volunteer address 2", "222222222", "1995-1-19", "male", "2015-2-06", "troop2");
+
+INSERT INTO volunteer (volunteer_id, volunteer_name, volunteer_address, volunteer_phone_number, date_of_birth, volunteer_gender, vetting_completion_date, troop_name)
+VALUES ("3", "Volunteer3", "Volunteer address 3", "333333333", "1995-1-19", "female", "2015-2-06", "troop3");
+
+INSERT INTO volunteer (volunteer_id, volunteer_name, volunteer_address, volunteer_phone_number, date_of_birth, volunteer_gender, vetting_completion_date, troop_name)
+VALUES ("4", "Volunteer4", "Volunteer address 4", "444444444", "1995-1-19", "male", "2015-2-06", "troop4");
 
 /* SECTION */
+INSERT INTO section (section_name, section_type, meeting_day, meeting_start_time, meeting_end_time, volunteer_id, troop_name)
+VALUES ("section0", "Junior", "0", "17:30", "18:30", "0", "troop0");
+
+INSERT INTO section (section_name, section_type, meeting_day, meeting_start_time, meeting_end_time, volunteer_id, troop_name)
+VALUES ("section1", "Junior", "1", "13:30", "14:30", "1", "troop1");
+
+INSERT INTO section (section_name, section_type, meeting_day, meeting_start_time, meeting_end_time, volunteer_id, troop_name)
+VALUES ("section2", "Pre-teen", "2", "16:30", "17:30", "2", "troop2");
+
+INSERT INTO section (section_name, section_type, meeting_day, meeting_start_time, meeting_end_time, volunteer_id, troop_name)
+VALUES ("section3", "Teen", "3", "19:30", "20:30", "3", "troop3");
+
+INSERT INTO section (section_name, section_type, meeting_day, meeting_start_time, meeting_end_time, volunteer_id, troop_name)
+VALUES ("section4", "Adolescent", "4", "21:30", "22:30", "4", "troop4");
 
 /* MEMBER */
+
+INSERT INTO member (member_id, member_name, member_address, date_of_birth, member_gender, parent_guardian, parent_guardian_phone_number, section_name)
+VALUES ("0", "Member 0", "Address of member 0", "1995-02-06", "female", "Parent 0", "000000000", "section0");
+
+INSERT INTO member (member_id, member_name, member_address, date_of_birth, member_gender, parent_guardian, parent_guardian_phone_number, section_name)
+VALUES ("1", "Member 1", "Address of member 1", "1995-02-06", "male", "Parent 1", "111111111", "section1");
+
+INSERT INTO member (member_id, member_name, member_address, date_of_birth, member_gender, parent_guardian, parent_guardian_phone_number, section_name)
+VALUES ("2", "Member 2", "Address of member 2", "1995-02-06", "female", "Parent 2", "222222222", "section2");
+
+INSERT INTO member (member_id, member_name, member_address, date_of_birth, member_gender, parent_guardian, parent_guardian_phone_number, section_name)
+VALUES ("3", "Member 3", "Address of member 3", "1995-02-06", "male", "Parent 3", "333333333", "section3");
+
+INSERT INTO member (member_id, member_name, member_address, date_of_birth, member_gender, parent_guardian, parent_guardian_phone_number, section_name)
+VALUES ("4", "Member 4", "Address of member 4", "1995-02-06", "female", "Parent 4", "444444444", "section4");
 
