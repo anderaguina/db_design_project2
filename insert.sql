e3eb810835e7424e233ff5fa5ed5842ae6fd5662
Ander Aguinagalde <ander@tisalabs.com>
2019-11-19 17:38:06 +0000

    Starting insert queries

--- insert.sql
+++ insert.sql
@@ -1,14 +1,41 @@
+/* TROOP */
 INSERT INTO troop (troop_name, troop_address, troop_website, troop_telephone_number)
-VALUES ("troop name 0", "troop address 0", "troop website 0", "phone 0");
+VALUES ("troop0", "Address from troop 0", "website.url@troop0.com", "000000000");
 
 INSERT INTO troop (troop_name, troop_address, troop_website, troop_telephone_number)
-VALUES ("troop name 1", "troop address 1", "troop website 1", "phone 1");
+VALUES ("troop1", "Address from troop 1", "website.url@troop1.com", "111111111");
 
 INSERT INTO troop (troop_name, troop_address, troop_website, troop_telephone_number)
-VALUES ("troop name 2", "troop address 2", "troop website 2", "phone 2");
+VALUES ("troop2", "Address from troop 2", "website.url@troop2.com", "222222222");
 
 INSERT INTO troop (troop_name, troop_address, troop_website, troop_telephone_number)
-VALUES ("troop name 3", "troop address 3", "troop website 3", "phone 3");
+VALUES ("troop3", "Address from troop 3", "website.url@troop3.com", "333333333");
 
 INSERT INTO troop (troop_name, troop_address, troop_website, troop_telephone_number)
-VALUES ("troop name 4", "troop address 4", "troop website 4", "phone 4");
+VALUES ("troop4", "Address from troop 4", "website.url@troop4.com", "444444444");
+
+/* SECTION TYPE */
+INSERT INTO section_type (code, descr)
+VALUES ("Junior", "This section type is specialized in age groups from 6 to 8 years");
+
+INSERT INTO section_type (code, descr)
+VALUES ("Pre-teen", "This section type is specialized in age groups from 9 to 11 years");
+
+INSERT INTO section_type (code, descr)
+VALUES ("Teen", "This section type is specialized in age groups from 12 to 14 years");
+
+INSERT INTO section_type (code, descr)
+VALUES ("Adolescent", "This section type is specialized in age groups from 15 to 17 years");
+
+/* NATIONAL EVENTS */
+
+/* MEMBER ATTENDS EVENT */
+
+/* EVENT IS ORGANIZED FOR SECTION TYPE */
+
+/* VOLUNTEER */
+
+/* SECTION */
+
+/* MEMBER */
+
